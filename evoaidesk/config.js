f4f2015af3b839fa95070536052ac7f8de8d0169
Shekhar D <ai.implementer@gmail.com>
2026-04-21 15:29:43 +0530

    Add files via upload

--- evoaidesk/config.js
+++ evoaidesk/config.js
@@ -1,6 +1,7 @@
 const API = {
-  n8n_workflow: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoaidesk_trigger_email",  
+  n8n_trigger_email_workflow: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoaidesk_trigger_email",  
   n8n_daily_report_workflow: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoAIDesk_daily_report",
   n8n_weekly_report_workflow: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoAIDesk_weekly_report",
-  n8n_summary_report_workflow: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoAIDesk_summary_report"
+  n8n_summary_report_workflow: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoAIDesk_summary_report",
+  n8n_activity_pipeline: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoaidesk_activities"
 };
