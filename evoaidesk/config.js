b85960af2a31fb3cfbdaf8b7c2b1567f90257d99
Shekhar D <ai.implementer@gmail.com>
2026-04-27 18:37:06 +0530

    Update config.js

--- evoaidesk/config.js
+++ evoaidesk/config.js
@@ -5,5 +5,5 @@ const API = {
   n8n_summary_report_workflow: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoAIDesk_summary_report",
   n8n_company_summary_report_workflow: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoAIDesk_company_summary_report",
   n8n_activity_pipeline: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoaidesk_activities",
-  n8n_company_intel_webhook: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoaidesk_company_intel",
+  n8n_company_intel_webhook: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook-test/evoaidesk_company_intel",
 };
