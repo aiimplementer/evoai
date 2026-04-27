bdf5c6e68171534e43b7dddf3d056c8fe3131b21
Shekhar D <ai.implementer@gmail.com>
2026-04-27 15:05:07 +0530

    Add files via upload

--- evoaidesk/config.js
+++ evoaidesk/config.js
@@ -1,9 +1,9 @@
 const API = {
-  n8n_trigger_email_workflow: "https://n8n.evoaiglobal.ai/webhook/evoaidesk_trigger_email",  
-  n8n_daily_report_workflow: "https://n8n.evoaiglobal.ai/webhook/evoAIDesk_daily_report",
-  n8n_weekly_report_workflow: "https://n8n.evoaiglobal.ai/webhook/evoAIDesk_weekly_report",
-  n8n_summary_report_workflow: "https://n8n.evoaiglobal.ai/webhook/evoAIDesk_summary_report",
-  n8n_company_summary_report_workflow: "https://n8n.evoaiglobal.ai/webhook-test/evoAIDesk_company_summary_report",
-  n8n_activity_pipeline: "https://n8n.evoaiglobal.ai/webhook/evoaidesk_activities",
-  n8n_company_intel_webhook: 'https://n8n.evoaiglobal.ai/webhook/evoaidesk_company_intel'
+  n8n_trigger_email_workflow: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoaidesk_trigger_email",  
+  n8n_daily_report_workflow: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoAIDesk_daily_report",
+  n8n_weekly_report_workflow: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoAIDesk_weekly_report",
+  n8n_summary_report_workflow: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoAIDesk_summary_report",
+  n8n_company_summary_report_workflow: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook-test/evoAIDesk_company_summary_report",
+  n8n_activity_pipeline: "https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoaidesk_activities",
+  n8n_company_intel_webhook: 'https://unsparse-sherman-nonfallaciously.ngrok-free.dev/webhook/evoaidesk_company_intel'
 };
